cc7143c9de16d419e912584e382bb2ec8296c6f2
WaiSham <lty8101919@gmail.com>
2026-04-30 19:58:05 +0800

    Update formatting

--- hwconf/trampa/vesc6/hw_60_rmesc.h
+++ hwconf/trampa/vesc6/hw_60_rmesc.h
@@ -4,17 +4,17 @@
 #define HW60_IS_RMESC
 
 // RMESC specific configuration overrides
-#define MCCONF_L_CURRENT_MAX                   10.0
-#define MCCONF_L_CURRENT_MIN                   -5.0
-#define MCCONF_L_MAX_ABS_CURRENT               30.0
-#define MCCONF_L_RPM_MAX                       120000.0
-#define MCCONF_L_RPM_MIN                       -120000.0
-#define MCCONF_FOC_F_ZV                        40000.0
-#define MCCONF_M_ENCODER_SIN_AMP               0.6
-#define MCCONF_M_ENCODER_SIN_OFFSET            1.0
-#define MCCONF_M_ENCODER_COS_AMP               0.6
-#define MCCONF_M_ENCODER_COS_OFFSET            1.0
-#define MCCONF_M_SENSOR_PORT_MODE              SENSOR_PORT_MODE_SINCOS
+#define MCCONF_L_CURRENT_MAX					10.0
+#define MCCONF_L_CURRENT_MIN					-5.0
+#define MCCONF_L_MAX_ABS_CURRENT				30.0
+#define MCCONF_L_RPM_MAX						120000.0
+#define MCCONF_L_RPM_MIN						-120000.0
+#define MCCONF_FOC_F_ZV							40000.0
+#define MCCONF_M_ENCODER_SIN_AMP				0.6
+#define MCCONF_M_ENCODER_SIN_OFFSET				1.0
+#define MCCONF_M_ENCODER_COS_AMP				0.6
+#define MCCONF_M_ENCODER_COS_OFFSET				1.0
+#define MCCONF_M_SENSOR_PORT_MODE				SENSOR_PORT_MODE_SINCOS
 
 #include "hw_60_core.h"
 
